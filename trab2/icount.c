a7076c74d3def7dd626f7a29b5be49d9310eba0b
Carlos Bravo <carlosh.serrado@gmail.com>
2023-05-09 10:04:28 -0300

    Change code format

--- trab2/icount.c
+++ trab2/icount.c
@@ -1,135 +1,119 @@
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <dirent.h>
 
-int walk_dir(const char *path, void (*func)(const char *))
-{
-    DIR *dirp;
-    struct dirent *dp;
-    char *p, *full_path;
-    int len;
-    /* abre o diretório */
-    if ((dirp = opendir(path)) == NULL)
-        return (-1);
-    len = strlen(path);
-    /* aloca uma área na qual, garantidamente, o caminho caberá */
-    if ((full_path = malloc(len + NAME_MAX + 2)) == NULL)
-    {
-        closedir(dirp);
-        return (-1);
-    }
-    /* copia o prefixo e acrescenta a ‘/’ ao final */
-    memcpy(full_path, path, len);
-    p = full_path + len;
-    *p++ = '/'; /* deixa “p” no lugar certo! */
-    while ((dp = readdir(dirp)) != NULL)
-    {
-        /* ignora as entradas “.” e “..” */
-        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
-            continue;
-        strcpy(p, dp->d_name);
-        /* “full_path” armazena o caminho */
-        (*func)(full_path);
-    }
-    free(full_path);
+int walk_dir(const char *path, void (*func)(const char *)) {
+  DIR *dirp;
+  struct dirent *dp;
+  char *p, *full_path;
+  int len;
+  /* abre o diretório */
+  if ((dirp = opendir(path)) == NULL) return (-1);
+  len = strlen(path);
+  /* aloca uma área na qual, garantidamente, o caminho caberá */
+  if ((full_path = malloc(len + NAME_MAX + 2)) == NULL) {
     closedir(dirp);
-    return (0);
+    return (-1);
+  }
+  /* copia o prefixo e acrescenta a ‘/’ ao final */
+  memcpy(full_path, path, len);
+  p = full_path + len;
+  *p++ = '/'; /* deixa “p” no lugar certo! */
+  while ((dp = readdir(dirp)) != NULL) {
+    /* ignora as entradas “.” e “..” */
+    if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) continue;
+    strcpy(p, dp->d_name);
+    /* “full_path” armazena o caminho */
+    (*func)(full_path);
+  }
+  free(full_path);
+  closedir(dirp);
+  return (0);
 }
 
-int walk_count_path(const char *fpath, int type)
-{
-    int count = 0;
-    void count_path(const char *fpath)
-    {
-        struct stat sb;
-        if (stat(fpath, &sb) == 0 && (sb.st_mode & S_IFMT) == type)
-        {
-            count++;
-            printf("%d. %s\n", count, fpath);
-        }
+int walk_count_path(const char *fpath, int type) {
+  int count = 0;
+  void count_path(const char *fpath) {
+    struct stat sb;
+    if (stat(fpath, &sb) == 0 && (sb.st_mode & S_IFMT) == type) {
+      count++;
+      printf("%d. %s\n", count, fpath);
     }
-    walk_dir(fpath, count_path);
-    return count;
+  }
+  walk_dir(fpath, count_path);
+  return count;
 }
 
-void print_count(const char *fpath, int count)
-{
-    switch (count)
-    {
+void print_count(const char *fpath, int count) {
+  switch (count) {
     case 0:
-        printf("Em %s não há arquivos do tipo especificado\n", fpath);
-        break;
+      printf("Em %s não há arquivos do tipo especificado\n", fpath);
+      break;
     case 1:
-        printf("Em %s há 1 arquivo do tipo especificado\n", fpath);
-        break;
+      printf("Em %s há 1 arquivo do tipo especificado\n", fpath);
+      break;
     default:
-        printf("Em %s há %d arquivos do tipo especificado\n", fpath, count);
-        break;
-    }
-    printf("\n");
+      printf("Em %s há %d arquivos do tipo especificado\n", fpath, count);
+      break;
+  }
+  printf("\n");
 }
 
-int main(int argc, char *argv[])
-{
-    int opt, type = S_IFREG;
-    opterr = 0;
-    int multiFlag = 0;
-    while ((opt = getopt(argc, argv, "rdlbc")) != -1)
-    {
-        if (multiFlag)
-        {
-            printf("Os modificadores são mutuamente exclusivos\n");
-            printf("Usage: %s [-rdlbc] [<dir> ...]\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-        multiFlag = 1;
-        switch (opt)
-        {
-        case 'r':
-            type = S_IFREG;
-            break;
-        case 'd':
-            type = S_IFDIR;
-            break;
-        case 'l':
-            type = S_IFLNK;
-            break;
-        case 'b':
-            type = S_IFBLK;
-            break;
-        case 'c':
-            type = S_IFCHR;
-            break;
-        default:
-            printf("Usage: %s [-rdlbc] [<dir> ...]\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
+int main(int argc, char *argv[]) {
+  int opt, type = S_IFREG;
+  opterr = 0;
+  int multiFlag = 0;
+  while ((opt = getopt(argc, argv, "rdlbc")) != -1) {
+    if (multiFlag) {
+      printf("Os modificadores são mutuamente exclusivos\n");
+      printf("Usage: %s [-rdlbc] [<dir> ...]\n", argv[0]);
+      exit(EXIT_FAILURE);
     }
-
-    int totalCount = 0;
-
-    if (optind == argc)
-    {
-        printf("Nenhum caminho especificado, usando o diretório atual\n");
-        char *path = ".";
-        int count = walk_count_path(path, type);
-        totalCount += count;
-        print_count(path, count);
+    multiFlag = 1;
+    switch (opt) {
+      case 'r':
+        type = S_IFREG;
+        break;
+      case 'd':
+        type = S_IFDIR;
+        break;
+      case 'l':
+        type = S_IFLNK;
+        break;
+      case 'b':
+        type = S_IFBLK;
+        break;
+      case 'c':
+        type = S_IFCHR;
+        break;
+      default:
+        printf("Usage: %s [-rdlbc] [<dir> ...]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
+  }
 
-    for (int i = optind; i < argc; i++)
-    {
-        char *path = argv[i];
-        printf("Acessando o diretório %s\n", path);
-        int count = walk_count_path(path, type);
-        totalCount += count;
-        print_count(path, count);
-    }
+  int totalCount = 0;
 
-    printf("Total de arquivos do tipo especificado: %d\n", totalCount);
-    return 0;
+  if (optind == argc) {
+    printf("Nenhum caminho especificado, usando o diretório atual\n");
+    char *path = ".";
+    int count = walk_count_path(path, type);
+    totalCount += count;
+    print_count(path, count);
+  }
+
+  for (int i = optind; i < argc; i++) {
+    char *path = argv[i];
+    printf("Acessando o diretório %s\n", path);
+    int count = walk_count_path(path, type);
+    totalCount += count;
+    print_count(path, count);
+  }
+
+  printf("Total de arquivos do tipo especificado: %d\n", totalCount);
+  return 0;
 }
--- trab2/nopen.c
+++ trab2/nopen.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int isopen(int fd) {
-    int result;
+  int result;
 
-    // Tenta pegar o status do descritor fd
-    struct stat buf;
-    result = fstat(fd, &buf);
+  // Tenta pegar o status do descritor fd
+  struct stat buf;
+  result = fstat(fd, &buf);
 
-    // Se a chamada ao sistema retornar -1, então ocorreu um erro
-    // Se o erro for EBADF, então o descritor não está aberto
-    if(result == -1 && errno == EBADF) return 0;
-    
-    // O descritor está aberto, a função retorna 1
-    return 1;
+  // Se a chamada ao sistema retornar -1, então ocorreu um erro
+  // Se o erro for EBADF, então o descritor não está aberto
+  if (result == -1 && errno == EBADF) return 0;
+
+  // O descritor está aberto, a função retorna 1
+  return 1;
 }
 
-int main (void){
-    int nopen, fd;
-    for (nopen = fd = 0; fd < getdtablesize(); fd++){
-        if (isopen (fd)) nopen++;
-    }
-    printf ("Existem %d descritores abertos\n", nopen);
-    return (0);
-} 
+int main(void) {
+  int nopen, fd;
+  for (nopen = fd = 0; fd < getdtablesize(); fd++) {
+    if (isopen(fd)) nopen++;
+  }
+  printf("Existem %d descritores abertos\n", nopen);
+  return (0);
+}
