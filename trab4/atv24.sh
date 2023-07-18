66b9db6c964a3e60b9a9be478410af6710773888
Carlos Bravo <carlosh.serrado@gmail.com>
2023-07-18 18:10:13 -0300

    Changed way of writing syslog

--- trab4/atv24.sh
+++ trab4/atv24.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Pega as ultimas 10 linhas de /var/log/syslog e sobreescreve o arquivo
-tail /var/log/syslog > /var/log/syslog
+tail /var/log/syslog | tee /var/log/syslog > /dev/null
